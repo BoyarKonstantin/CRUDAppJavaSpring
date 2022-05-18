cfacfed6d62e873c7c3d4837cea9a6c59e119e70
BoyarKonstantin <97537403+BoyarKonstantin@users.noreply.github.com>
2022-05-18 14:27:58 +0300

    Update BlogDAO.java

--- src/main/java/MVC/Blog/Konstantin/DAO/BlogDAO.java
+++ src/main/java/MVC/Blog/Konstantin/DAO/BlogDAO.java
@@ -3,48 +3,124 @@ package MVC.Blog.Konstantin.DAO;
 import MVC.Blog.Konstantin.models.BlogModel;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class BlogDAO {
     private static int ID;
-    private List<BlogModel> blogList;
 
-    {
-        blogList = new ArrayList<BlogModel>();
+    private static final String URL = "jdbc:postgresql://localhost:5432/Blog";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "Dod900ls";
 
-        blogList.add(new BlogModel(++ID, "TextText",
-                "Author1", "asklfnlaksjk"));
-        blogList.add(new BlogModel(++ID, "TextText1",
-                "Author2", "asklfnlaksjk"));
-        blogList.add(new BlogModel(++ID, "TextText2",
-                "Author3", "asklfnlaksjk"));
-        blogList.add(new BlogModel(++ID, "TextText3",
-                "Author4", "asklfnlaksjk"));
 
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
+
     public List<BlogModel> index() {
+        List<BlogModel> blogList = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM blog"; // Обращаемся к Postgres таблице
+            statement.executeQuery(SQL);
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                BlogModel blogModel = new BlogModel();
+
+                blogModel.setId(resultSet.getInt("id"));
+                blogModel.setAuthor(resultSet.getString("author"));
+
+                blogModel.setTitle(resultSet.getString("title"));
+                blogModel.setText(resultSet.getString("text"));
+
+                blogList.add(blogModel);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return blogList;
     }
 
     public BlogModel show(int id) {
-        return blogList.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        BlogModel blogModel = null;
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("SELECT * FROM blog where id=?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+
+            blogModel = new BlogModel();
+
+            blogModel.setId(resultSet.getInt("id"));
+            blogModel.setText(resultSet.getString("text"));
+
+            blogModel.setAuthor(resultSet.getString("author"));
+            blogModel.setTitle(resultSet.getString("title"));
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return blogModel;
     }
 
     public void save(BlogModel blogModel) {
-        blogModel.setId(++ID);
-        blogList.add(blogModel);
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO blog VALUES(1, ?, ?, ?)");
+
+            preparedStatement.setString(1, blogModel.getAuthor());
+            preparedStatement.setString(2, blogModel.getTitle());
+            preparedStatement.setString(3, blogModel.getText());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void update(int id, BlogModel updateBlogModel) {
-        BlogModel modelToBeUpdated = show(id);
-        modelToBeUpdated.setAuthor(updateBlogModel.getAuthor());
-        modelToBeUpdated.setText(updateBlogModel.getText());
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("UPDATE blog SET author = ?, title = ?, text = ? WHERE id=?");
+            preparedStatement.setInt(4, updateBlogModel.getId());
+            preparedStatement.setString(2, updateBlogModel.getTitle());
+            preparedStatement.setString(1, updateBlogModel.getAuthor());
+            preparedStatement.setString(3, updateBlogModel.getText());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void delete(int id) {
-        blogList.removeIf(blog -> blog.getId() == id);
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM blog WHERE id=?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
